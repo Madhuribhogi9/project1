5184faf6e3e4a2162f728d0216ed8c07f1a1324f
madhuribhogi9 <madhubhogi4@gmail.com>
2022-04-17 21:48:29 -0700

    using intellij1

---
+++ src/main/java/test1.java
@@ -0,0 +1,13 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test1
+{
+
+        @Test
+                public void test1l()
+        {
+            Reporter.log("hai",true);
+            Reporter.log("no",true);
+    }
+}
---
+++ src/main/java/test2.java
@@ -0,0 +1,11 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test2
+{
+    @Test
+    public void test2l()
+    {
+        Reporter.log("hello",true);
+    }
+}
